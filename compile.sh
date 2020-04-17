765f10e24210ad174f48e55bfd7626489fc616fd
Bryan Lai <bryanlais@gmail.com>
2020-04-17 13:44:05 +0800

    Update compile.sh to support lualatex in %!TeX

--- compile.sh
+++ compile.sh
@@ -17,14 +17,30 @@ echo "$FILES"
 
 for file in $FILES; do
     echo "### Compiling \`$file\` ..."
-    cd "$(dirname "$file")" || exit 1
 
-    latexmk \
-        -xelatex \
-        -synctex=1 \
-        -interaction=nonstopmode \
-        -shell-escape \
-        "$(basename "$file")"
+    cd "$(dirname "$file")" || exit 1
+    filename=$(basename "$file")
+
+    # Process magic comment
+    tex_program=$(
+        grep "%[ ]*\!TeX[ ]*TS-program[ ]*=[ ]*" "$filename" \
+        | cut -d '=' -f2 | xargs
+    )
+
+    if [[ $tex_program == pdflatex ]] \
+    || [[ $tex_program == xelatex ]] \
+    || [[ $tex_program == lualatex ]]; then
+        latexmk \
+            "-$tex_program" \
+            -synctex=1 \
+            -interaction=nonstopmode \
+            -shell-escape \
+            "$filename"
+    else
+        eval "$tex_program" \
+            "$filename"
+    fi
+
 
     cd "$DIR" || exit 1
 done
