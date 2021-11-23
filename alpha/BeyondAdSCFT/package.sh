6c6ee26a7d7b5512903ec895370318bbb40f0fd3
Bryan Lai <bryanlais@gmail.com>
2021-11-23 11:26:30 +0800

    Restructure notes on holography beyond AdS

---
+++ alpha/BeyondAdSCFT/package.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# package LaTeX source for distribution
+
+# FILENAME="${1%.tex}"; shift
+FILENAME=beyondAdS
+TARGET=release
+
+[[ -r "$FILENAME".tex ]] \
+    || { echo "# Couldn't access $FILENAME.tex"; exit 1; }
+
+mkdir "$TARGET" || true
+# echo "*" > "$TARGET/.gitignore"
+
+PREAMBLE=preamble.tex
+sed-latex "$PREAMBLE" > "$TARGET/$PREAMBLE"
+
+rsync \
+    --exclude='.git*' --recursive --copy-links \
+    --update --partial \
+    --progress --verbose \
+    "$@" \
+    "$FILENAME.tex" \
+    "$FILENAME.bib" \
+    "$FILENAME.bbl" \
+    "$FILENAME.pdf" \
+    "img/" \
+    "README.md" \
+    "$TARGET"
+
+# cd "$TARGET" || exit
+# zip -r "$FILENAME.zip" .
